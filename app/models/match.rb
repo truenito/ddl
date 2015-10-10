46c2d213f404e2d0bca2dcd4ca06175c229c868b
Gabriel González Mañaná <gabriel@estilopanda.com>
2015-10-10 18:52:49 -0400

    Refactor, rework, DRY up models.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,98 +1,120 @@
+# Match class, models a match (game) in the league, a match starts empty and
+# finishes with 10 players, 2 teams (one winner and one loser).
 class Match < ActiveRecord::Base
-
-  has_many :users, :through => :match_tokens
+  has_many :users, through: :match_tokens
   has_many :teams
   has_many :match_tokens
 
+  # Interface to create teams.
   def create_teams
-    # Get relevant players data and balance for team creation
-    players_pool = self.users.pluck(:user_id, :rating)
+    # Get relevant players data and balance for team creation.
+    players_pool = users.pluck(:user_id, :rating)
     balanced_teams_hash = balance_players(players_pool)
 
     # Create real teams for match with the balanced team hash.
-    Team.create_for_match(balanced_teams_hash, self.id)
+    Team.create_for_match(balanced_teams_hash, id)
     save_users_and_attributes(:id, :name, :rating, :status,
                               :facebook_link, :donator,
                               :win_count, :lose_count)
   end
 
-  # Balance algorithm (team distribution).
-  # TODO @truenito: Divide into smaller methods and make it cleaner.
+  # Balance interface (team distribution).
   def balance_players(players_pool_array)
-    balanced = false
-    min_rating_separation = 3
-    shuffles = 0
-    radiant_team, dire_team = [], []
+    @balanced = false
+    @min_separation = 3
+    @shuffles = 0
 
-    while balanced == false
+    while @balanced == false
       players_pool_array.shuffle!
-      shuffles += 1
-      radiant_team_array, radiant_team_avg = players_pool_array.first(5), 0
-      dire_team_array, dire_radiant_team_avg = players_pool_array.last(5), 0
+      @shuffles += 1
+      distribute_players players_pool_array
 
-      radiant_team_array.each{|player| radiant_team_avg += player[1] }
-      dire_team_array.each{|player| dire_radiant_team_avg += player[1] }
-
-      (radiant_team_avg - dire_radiant_team_avg).
-      abs < min_rating_separation ? balanced = true : balanced = false
-      if shuffles > 1000
-        min_rating_separation += 3
-        shuffles = 0
-      end
+      check_separation @radiant_avg, @dire_avg, @min_separation
     end
 
-    radiant_team_array.each{|player| radiant_team << User.find(player.first)}
-    dire_team_array.each{|player| dire_team << User.find(player.first)}
-
-    return { radiant_team: radiant_team, dire_team: dire_team}
+    assign_teams @radiant_array, @dire_array
   end
 
-  # Match commit interface.
+  # Distributes players between radiant and dire from a shuffled player array.
+  def distribute_players(players_pool_array)
+    @radiant_array, @radiant_avg = players_pool_array.first(5), 0
+    @dire_array, @dire_avg = players_pool_array.last(5), 0
+
+    @radiant_array.each { |player| @radiant_avg += player[1] }
+    @dire_array.each { |player| @dire_avg += player[1] }
+  end
+
+  # Checks if team rating averages meet minimum separation, if not reshuffles,
+  # additionally, checks if if it's needed to increase the minimun separation.
+  def check_separation(radiant_avg, dire_avg, min_separation)
+    if (radiant_avg - dire_avg).abs < min_separation
+      @balanced = true
+    else
+      @balanced = false
+      if @shuffles > 1000
+        @min_separation += 3
+        @shuffles = 0
+      end
+    end
+  end
+
+  # Assigns the players to a temp team hash in order to create the real teams.
+  def assign_teams(radiant_array, dire_array)
+    dire = []
+    radiant = []
+    radiant_array.each { |player| radiant << User.find(player.first) }
+    dire_array.each { |player| dire << User.find(player.first) }
+
+    { radiant: radiant, dire: dire }
+  end
+
+  # Commit and finalize the Match if enough votes have been processed.
+  # 6 votes is enough to declare a winner.
   def commit_match
-    if committable?
-      result = @freq.reject{|k,v|v < 6}.keys.first # get the result with 6 votes.
-      if result == 'radiant' || result == 'dire'
-        winner_team = self.teams.where(side: result)
-        loser_team = (self.teams - winner_team).first
-        winner_team = winner_team.first
+    return false unless committable?
+    result = @freq.reject { |_k, v| v < 6 }.keys.first
+    if result == 'radiant' || result == 'dire'
+      winner_team = teams.where(side: result)
+      loser_team = (teams - winner_team).first
+      winner_team = winner_team.first
 
-        update_player_stats(winner_team, loser_team, result)
-        finalize_match(winner_team, result)
-      else
-        cancel_match
-      end
+      update_player_stats(winner_team, loser_team, result)
+      finalize_match(winner_team, result)
+    else
+      cancel_match
     end
   end
 
-  # TODO @truenito: Split into little methods and make it an interface.
+  # TODO: @truenito: Split into little methods and make it an interface.
   def update_player_stats(winner_team, loser_team, result)
     # update win counts
-    winner_team.users.each{|u| c = u.win_count + 1; u.win_count = c; u.save!}
-    loser_team.users.each{|u| c = u.lose_count + 1; u.lose_count = c; u.save!}
+    winner_team.users.each { |u| c = u.win_count + 1; u.win_count = c; u.save! }
+    loser_team.users.each { |u| c = u.lose_count + 1; u.lose_count = c; u.save! }
 
-    # Updates ratings.
+    # Check how much player rating going to change.
     rating_change = 0
-    rating_change = (Team.rating_change((winner_team.users.sum(:rating) / 5), (loser_team.users.sum(:rating) / 5)))
+    rating_change = (Team.rating_change((winner_team.users.sum(:rating) / 5),
+                                        (loser_team.users.sum(:rating) / 5)))
 
-    # Logic: Depending on who won/lost substract corresponding amount from both teams.
+    # Decrease losing team's rating and increase winner team's rating
+    # based on the rating change.
     if result == 'radiant'
-      winner_team.users.each{|u| c = u.rating + rating_change[:dire]; u.rating = c; u.save!}
-      loser_team.users.each{|u| c = u.rating - rating_change[:dire]; u.rating = c; u.save!}
+      winner_team.users.each { |u| c = u.rating + rating_change[:dire]; u.rating = c; u.save! }
+      loser_team.users.each { |u| c = u.rating - rating_change[:dire]; u.rating = c; u.save! }
     else
-      winner_team.users.each{|u| c = u.rating + rating_change[:radiant]; u.rating = c; u.save!}
-      loser_team.users.each{|u| c = u.rating - rating_change[:radiant]; u.rating = c; u.save!}
+      winner_team.users.each { |u| c = u.rating + rating_change[:radiant]; u.rating = c; u.save! }
+      loser_team.users.each { |u| c = u.rating - rating_change[:radiant]; u.rating = c; u.save! }
     end
   end
 
   # Saves match result information for teams and sets "ended" status.
-  def finalize_match(winner_team, result)
+  def finalize_match(result)
     if result == 'radiant'
       winner_team_bool = false
     elsif result == 'dire'
       winner_team_bool = true
     end
-    self.match_tokens.each{|t| t.result = winner_team_bool; ; t.save!}
-    self.winner_team = winner_team_bool
+    match_tokens.each { |t| t.result = winner_team_bool; t.save! }
 
     self.status = 'ended'
     self.save!
@@ -104,44 +126,55 @@ class Match < ActiveRecord::Base
     self.winner_team = nil
     self.save!
 
-    self.match_tokens.destroy_all
+    match_tokens.destroy_all
   end
 
   # Saves frozen user attributes for future reference i.e: actual rating at the
   # time the match happened.
   def save_users_and_attributes(*stats)
-    players_pool_with_stats = self.users.select(stats)
-    self.users_and_stats  = players_pool_with_stats.as_json
+    players_pool_with_stats = users.select(stats)
+    self.users_and_stats = players_pool_with_stats.as_json
   end
 
   # match statuses/interactions.
   def votes_number
-    self.match_tokens.select{|token| token.result != nil}.count
+    match_tokens.select { |token| !token.result.nil? }.count
   end
 
-  # Check if match is joinable by players/waiting for players.
-  def joinable? ; self.status == 'waiting'; end
+  # Checks if match is joinable by players/waiting for players.
+  def joinable?
+    status == 'waiting'
+  end
   alias_method :waiting?, :joinable?
-  # Check if match is being played at the moment.
-  def playing? ; self.status == 'playing' ; end
-  # Check if  match is canceled.
-  def canceled? ; self.status == 'canceled'; end
-  # Check if match has ended.
-  def ended? ; self.status == 'ended'; end
-  # Check if match is leaveable at the moment.
-  def leavable? ; !(self.status == 'playing'); end
+
+  # Checks if match is leaveable at the moment.
+  def leavable?
+    !(status == 'playing')
+  end
+
+  # Checks if match is being played at the moment.
+  def playing?
+    status == 'playing'
+  end
+
+  # Checks if  match is canceled.
+  def canceled?
+    status == 'canceled'
+  end
+
+  # Checks if match has ended.
+  def ended?
+    status == 'ended'
+  end
 
   # Checks if the match has ended and if a team has enough result votes to
   # commit it (save the results of the match and end it).
   def committable?
-    return false if self.status == 'ended' || self.status == 'canceled'
-    voted_results = self.match_tokens.map(&:result)
+    return false if status == 'ended' || status == 'canceled'
     @freq = Hash.new(0)
-    voted_results.each do |v|
-      @freq[v] += 1
-    end
+    voted_results = match_tokens.map(&:result)
+    voted_results.each { |v| @freq[v] += 1 }
 
     @freq['radiant'] >= 6 || @freq['canceled'] >= 6 || @freq['dire'] >= 6 ? true : false
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,37 +1,39 @@
+# User class, all users registered entities, a user may or may not be
+# authenticated with Facebook (but needs to in order to play in the league),
+# all users are players.
 class User < ActiveRecord::Base
   include UserPrivileges
   extend UserRankings
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
-  has_many :match_tokens
-  has_many :matches, :through => :match_tokens
   has_and_belongs_to_many :teams
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+  has_many :match_tokens
+  has_many :matches, through: :match_tokens
 
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
-  validates_length_of :description, :minimum => 10, :maximum => 200, :allow_blank => false
-
+  validates :description, length: { minimum: 10, maximum: 200, allow_blank: false }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
     end
   end
 
   def leave_match(match)
-    token = match.match_tokens.where(user_id: self.id).first
-    token.delete
+    match.match_tokens.find_by(user_id: id).delete
   end
 
   def win_rate
-    if self.lose_count + self.win_count.to_f == 0
+    if lose_count + win_count.to_f == 0
       0
     else
-      (self.win_count.to_f / (self.lose_count + self.win_count.to_f) * 100).to_i
+      (win_count.to_f / (lose_count + win_count.to_f) * 100).to_i
     end
   end
 
@@ -39,20 +41,23 @@ class User < ActiveRecord::Base
     object_label_method :to_label
   end
 
-  def to_label
-    "#{name} - #{email}"
-  end
-
   def admin?
     admin
   end
 
   def facebook_authed?
-    self.provider == 'facebook'
+    provider == 'facebook'
   end
 
   def joinable?
-    facebook_authed? && !self.match_tokens.where(result: nil).any?
+    facebook_authed? && !match_tokens.where(result: nil).any?
   end
 
+  def unjoinable?
+    !facebook_authed? || matches.last.playing?
+  end
+
+  def to_label
+    "#{name} - #{email}"
+  end
 end
