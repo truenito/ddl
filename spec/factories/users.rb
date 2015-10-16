89c45ffb33141e4040181ed46a70135bf8d48bd0
Gabriel González Mañaná <gabriel@estilopanda.com>
2015-10-16 11:39:51 -0400

    Add user factory to mock users for tests.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,21 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :user do
-    sequence(:name) { |n| "cerito#{n}" }
-    sequence(:email) { |n| "cerito#{n}@ddl.com" }
-    password 'changeme'
-    password_confirmation 'changeme'
+    password = Faker::Internet.password
+    id  { rand 1_000..2_000_000 }
+    sequence(:name) { |n| Faker::Name.first_name + "#{n}" }
+    email { Faker::Internet.email }
+    password {  password }
+    password_confirmation { password }
+    rating { rand 3300..3900 }
 
-    # factory :user_with_image do
-    #   avatar { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'images', 'guaremate.png')) }
+    factory :authed_user do
+      provider { 'facebook' }
+    end
+
+    factory :non_authed_user do
+      provider { nil }
+    end
   end
 end
